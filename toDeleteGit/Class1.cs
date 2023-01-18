30cfe413941d1ea65eb4b5b0439b8a7a41b661f3
Egor Kuleshov <Egor.Kuleshov@comindware.ru>
2023-01-18 14:19:22 +0300

    SomeChanges5

--- toDeleteGit/Class1.cs
+++ toDeleteGit/Class1.cs
@@ -6,4 +6,6 @@ public class Class1
     public int SomeChanges2 { get; set; }
     public int SomeChanges3 { get; set; }
     public int SomeChanges4 { get; set; }
+    public int SomeChanges5 { get; set; }
 }
+
