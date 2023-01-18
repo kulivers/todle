7c97d7e9aeaf33f74a6710e27542c8b5d36d773b
Egor Kuleshov <Egor.Kuleshov@comindware.ru>
2023-01-18 14:30:37 +0300

    squash! i

--- toDeleteGit/Class1.cs
+++ toDeleteGit/Class1.cs
@@ -8,4 +8,5 @@ public class Class1
     public int SomeChanges4 { get; set; }
     public int SomeChanges5 { get; set; }
     public int SomeChanges6 { get; set; }
+    public int SomeChanges7 { get; set; }
 }
